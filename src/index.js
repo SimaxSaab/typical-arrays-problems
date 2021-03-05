e20224877635d00c3e17d7ded9511f73413496ca
SimaxSaab <minoreat@gmail.com>
2021-03-05 09:34:55 +0200

    init: ready

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0) return 0;
+  let min = array[0];
+  for(let i = 1, len = array.length; i < len; i++) {
+    if(array[i] < min) min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0) return 0;
+  let max = array[0];
+  for(let i = 1, len = array.length; i < len; i++) {
+    if(array[i] > max) max = array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(typeof array == 'undefined' || array.length == 0) return 0;
+  let avg = 0, len = array.length;
+  for(let i = 0; i < len; i++) {
+    avg += array[i];
+  }
+  return avg / len;
 }
